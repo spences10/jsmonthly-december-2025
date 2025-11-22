1fc7894cbb2735aa01da7152780c90e37b7e0bed
spences10 <spences10apps@gmail.com>
2025-11-22 16:49:51 +0000

    feat: Add about me slide component with dynamic content and transitions

---
+++ src/slides-custom/about-me.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { Transition } from '@animotion/core'
+
+	let { name = '', title = '', points = [], image = '' } = $props()
+</script>
+
+<div class="flex items-center gap-20">
+	<div class="flex flex-col text-left">
+		{#if name}
+			<Transition visible entry="slide-in-from-left" duration={0.6}>
+				<p class="text-9xl font-black">{name}</p>
+			</Transition>
+		{/if}
+
+		{#if points.length > 0}
+			<div class="mt-4 flex flex-col gap-2 text-4xl font-semibold">
+				{#each points as point, i}
+					<Transition
+						visible
+						entry="slide-in-from-left"
+						duration={0.5}
+						delay={0.2 + i * 0.1}
+					>
+						<p>- {point}</p>
+					</Transition>
+				{/each}
+			</div>
+		{/if}
+	</div>
+
+	{#if image}
+		<Transition visible entry="scale-in" duration={0.7} delay={0.1}>
+			<img
+				src={image}
+				alt={name}
+				class="h-80 w-80 rounded-full object-cover ring-4 ring-gray-200"
+			/>
+		</Transition>
+	{/if}
+</div>
--- src/slides-custom/title-slide.svelte
+++ src/slides-custom/title-slide.svelte
@@ -4,8 +4,8 @@
 	let {
 		title = 'Your Talk Title Here',
 		subtitle = '',
-		name = 'Your Name',
-		event = 'Event Name',
+		name = '',
+		event = '',
 		date = '',
 	} = $props()
 </script>
@@ -28,15 +28,19 @@
 	</Transition>
 {/if}
 
-<Transition
-	visible
-	entry="scale-in"
-	duration={0.6}
-	delay={0.5}
-	order={2}
->
-	<div class="mt-16 flex flex-col gap-4 text-3xl opacity-70">
-		<p>{name}</p>
-		<p>{event}{#if date}{' '}-{' '}{date}{/if}</p>
-	</div>
-</Transition>
+{#if name || event || date}
+	<Transition
+		visible
+		entry="scale-in"
+		duration={0.6}
+		delay={0.5}
+		order={2}
+	>
+		<div class="mt-16 flex flex-col gap-4 text-3xl opacity-70">
+			{#if name}<p>{name}</p>{/if}
+			{#if event || date}
+				<p>{event}{#if event && date}{' '}-{' '}{/if}{date}</p>
+			{/if}
+		</div>
+	</Transition>
+{/if}
